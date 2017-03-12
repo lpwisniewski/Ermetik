2d00861142d3caf7a412fd16ba9f74e301f5d428
bl00m <leon.pierre@openmailbox.org>
2017-03-12 14:26:38 +0100

    Change import & simple object test

--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,18 @@ import 'babel-polyfill'
 // import './styles/main'
 // TODO CSS
 
-import React from 'react'
-import { render } from 'react-dom'
+import { h, render, Component } from 'preact';
 import { Router, hashHistory } from 'preact-router'
 
+// Simple object to test preact
+class Clock extends Component {
+    render() {
+        let time = new Date().toLocaleTimeString();
+        return <span>{ time }</span>;
+    }
+}
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.querySelector('[role=application]')
-  render("Hello, world!", root)
+  render(<Clock />, root)
 })
