23881b7f2e304f60f2b70a573ef48b04733d50fc
bl00m <leon.pierre@openmailbox.org>
2017-03-12 17:37:45 +0100

    Simple monologue chat

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,65 @@
 import 'babel-polyfill'
 
-// import './styles/main'
-// TODO CSS
+import './styles/main'
 
 import { h, render, Component } from 'preact';
 import { Router, hashHistory } from 'preact-router'
 
-// Simple object to test preact
-class Clock extends Component {
-    render() {
-        let time = new Date().toLocaleTimeString();
-        return <span>{ time }</span>;
-    }
+class TchatApp extends Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = {msgs: [], text: ''};
+  }
+
+  render() {
+    return (
+      <main>
+        <MsgFlux msgs={this.state.msgs} />
+        <form onSubmit={this.handleSubmit}>
+          <input onChange={this.handleChange} value={this.state.text} placeholder="Message..." />
+          <button>Envoyer</button>
+        </form>
+      </main>
+    );
+  }
+
+  handleChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var newMsg = {
+      author: 'user',
+      date: new Date().toLocaleTimeString(),
+      text: this.state.text
+    };
+    this.setState((prevState) => ({
+      msgs: prevState.msgs.concat(newMsg),
+      text: ''
+    }));
+  }
+}
+
+class MsgFlux extends Component {
+  render() {
+    return (
+      <ul>
+        {this.props.msgs.map(msg => (
+          <li>
+            <span class="author">{msg.author}: </span>
+            <span class="date">{msg.date}</span>
+            <span>{msg.text}</span>
+          </li>
+        ))}
+      </ul>
+    );
+  }
 }
 
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.querySelector('[role=application]')
-  render(<Clock />, root)
+  render(<TchatApp />, root)
 })
